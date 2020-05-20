20c612755468e7645e465f6c6d069ab43e3702f9
sumit <sumit.choudhary@eclinicalworks.com>
2020-05-20 18:12:49 -0400

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -23,6 +23,10 @@ const create_item = () => {
   item.addEventListener("dragstart", event => event.dataTransfer.setData('text',event.target.id));
   item.addEventListener("dragend", event => event.dataTransfer.clearData());
 
+  let input = document.createElement('input');
+  item.appendChild(input);
+  
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
